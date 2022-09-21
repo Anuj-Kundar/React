6275e0e14f1934b4153e3180d046aceadbc98875
Anuj Kundar <anujkundar003@gmail.com>
2022-09-21 23:59:20 +0530

    added searchbox

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Card from './Card';
 
-const CardList=({robots})=>{
-    const cardsArray = robots.map((user,i)=>{
-        return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
-    })
-    return(
+const CardList = ({ robots }) => {
+
+    return (
         <>
-            {cardsArray}
+            {
+                robots.map((user, i) => {
+                    return (<Card
+                        key={i}
+                        id={robots[i].id}
+                        name={robots[i].name}
+                        email={robots[i].email}
+                    />
+                    );
+                })
+            }
         </>
     );
 }
