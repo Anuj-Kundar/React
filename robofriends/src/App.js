ab52091a388c19fcfe3278dfbefe5d0651e84edb
Anuj Kundar <anujkundar003@gmail.com>
2022-09-26 23:10:32 +0530

    added fetch() to fetch user data

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from './robots';
 import 'tachyons';
 import './App.css';
 
@@ -10,22 +9,27 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => {this.setState({ robots: users})});
+    }
 
-    onSearchChange=(event)=> {
-        this.setState({searchfield: event.target.value})
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
     }
     render() {
-        const filteredRobots=this.state.robots.filter(robots=>{
+        const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
         return (
             <div className="tc">
                 <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={this.onSearchChange} />
                 <CardList robots={filteredRobots} />
             </div>
 
